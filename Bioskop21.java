5c9a6d0f33597fc13214ede1ec06a991cadbcf52
Nurwahibzy <nurwahibzakkiy@gmail.com>
2023-11-02 09:36:07 +0700

    Menambahkan foreach untuk mencetak nama pada setiap baris

--- Bioskop21.java
+++ Bioskop21.java
@@ -1,4 +1,5 @@
 package Jobsheet10;
+
 public class Bioskop21 {
     public static void main(String[] args) {
         String[][] penonton = new String[4][2];
@@ -16,15 +17,13 @@ public class Bioskop21 {
         System.out.printf("%s \t %s \n", penonton[2][0], penonton[2][1]);
         System.out.printf("%s \t %s \n", penonton[3][0], penonton[3][1]);
 
-        
         System.out.println(penonton.length);
         for (int i = 0; i < penonton.length; i++) {
-            System.out.println("Panjang baris ke-" + (i+1)+ ": " + penonton[i].length);
+            System.out.println("Panjang baris ke-" + (i + 1) + ": " + penonton[i].length);
         }
-        System.out.println("Penonton pada baris ke-3: ");
-        for (String i : penonton[2]) {
-            System.out.println(i);
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Penonton pada baris ke-" + (i+1) + "; " + String.join(", ", penonton[i]));
+
         }
     }
-
 }
